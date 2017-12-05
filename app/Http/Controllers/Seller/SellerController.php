93c52dd94449f933899a75c8c802aa59b1f18d03
Aung Kyaw <aung.kyaw@miaki.co>
2017-12-05 15:05:36 +0630

    first commit

---
+++ app/Http/Controllers/Seller/SellerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sellers = Seller::has('products')->get();
+
+        return response()->json(['data' => $sellers], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $seller = Seller::has('products')->findOrFail($id);
+        
+        return response()->json(['data' => $seller], 200);
+    }
+}
