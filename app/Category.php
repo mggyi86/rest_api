75ac699704a4bc17f7d778b1e3619b2f993e3c7f
Aung Kyaw <aung.kyaw@miaki.co>
2018-03-20 14:07:27 +0630

    first commit

--- app/Category.php
+++ app/Category.php
@@ -4,14 +4,14 @@ namespace App;
 
 use App\Product;
 use Illuminate\Database\Eloquent\Model;
-use App\Transformers\ProductTransformer;
+use App\Transformers\CategoryTransformer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     use SoftDeletes;
 
-    public $transformer = ProductTransformer::class;
+    public $transformer = CategoryTransformer::class;
     protected $dates = ['deleted_at'];
     protected $fillable = ['name', 'description'];
     protected $hidden = [
